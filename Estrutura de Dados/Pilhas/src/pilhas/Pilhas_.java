ded35ae1d4b5bd1ef7d36d5855cf507f488a5e34
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-13 12:20:22 -0300

    ada

--- Estrutura de Dados/Pilhas/src/pilhas/Pilhas_.java
+++ Estrutura de Dados/Pilhas/src/pilhas/Pilhas_.java
@@ -2,5 +2,10 @@
 package pilhas;
 
 public class Pilhas_ {
+    //Pilhas = lista ordenada (ordem de inserção de elementos) = o último elemento inserido será o primeiro a ser tirado
+    // LIFO = last in first out (último que entrou será o 1° a sair) ---------------- FILO = First in last out (O 1° que entrou será o último a sair)
+    // Possui apenas 1 extremidade, a extremidade em que insiro elementos é a mesma que eu retiro elementos
+    // extreminada denominada como TOP ou TOPO
+    // ex: Pilha de pratos, vc enxuga o primeiro de cima que foi o último a ser lavado
     
 }
