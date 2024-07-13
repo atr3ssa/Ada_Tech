da093201c206e92519f58a70eddee2155b09142e
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-13 12:33:41 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Pilha.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Pilha.java
@@ -1,7 +1,7 @@
 //pacote
-package pilhas;
+package dataStructure;
 
-public class Pilhas_ {
+public class Pilha {
     //Pilhas = lista ordenada (ordem de inserção de elementos) = o último elemento inserido será o primeiro a ser tirado
     // LIFO = last in first out (último que entrou será o 1° a sair) ---------------- FILO = First in last out (O 1° que entrou será o último a sair)
     // Possui apenas 1 extremidade, a extremidade em que insiro elementos é a mesma que eu retiro elementos
