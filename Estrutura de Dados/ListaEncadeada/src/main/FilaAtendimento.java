2537b56261c12484e7396851f3aa444d6276900f
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-14 11:44:37 -0300

    ada

---
+++ Estrutura de Dados/ListaEncadeada/src/main/FilaAtendimento.java
@@ -0,0 +1,10 @@
+// pacote
+package main;
+
+// PROBLEMA: Crie um programa JAVA que que simule uma fila de atendimento e os clienetes entrem na fila e sejam atendidos um por um
+public class FilaAtendimento {
+    
+    public static void main(String[] args) {
+        
+    }
+}
