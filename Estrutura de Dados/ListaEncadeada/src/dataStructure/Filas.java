f38f07f4c07bab1f28bd6d3a8aed773115828833
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-14 09:16:47 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Filas.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Filas.java
@@ -2,6 +2,24 @@
 package dataStructure;
 
 
-public class Filas {
+public class Filas { // FILAS
+    // FILAS = 1° elemento inserido será o 1° a ser tirado
+    // FIFO = First in First out ---- LILO = Last in Last out
+    // ex: Fila de atendimento bancário
+    // Possui 2 extremidades: em 1: É feita a inserção de elementos (fim da fila); em outra: É feita a remoção de elementos (ínicio da fila);
+    // Problemas resolvidos: Escalonamento de tarefas por ordem de chegada
+    // Tempo de espera de clientes em Call Center
+    // Definição do número de caixas de supermercado
     
+    // Principais Operações:
+    
+    // Add Item (queue)
+    // Remover item (dequeue)
+    // Imprimir (print)
+    
+    // Operação Opcional:
+    
+    // Primeiro da fila (getFirst)
+    // Último da fila (getLast)
+    // tamanho da fila (getLength)
 }
