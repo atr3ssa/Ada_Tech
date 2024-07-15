e620fbe805d2a0a8d9f34ab2fb8ae153320bb20a
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-15 15:46:37 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
@@ -15,13 +15,14 @@ public class Arvore { // Árvore
         // CHEIA = Quando cada nó possui 2 ou 0 filhos
         // ÁRVORE NÃO CHEIA = nó possui 1 filho
         // ÁRVORE PERFEITA = Uma árvore cheia onde todas as FOLHAS estão no mesmo nível - em cada nível = 2^n 
-            // nível 1 = 1 - nível 2 = 2 - nível 3 = 4 - nível 4 = 8 ... nível 10 = 1024
+            // nível 0 = 1 - nível 1 = 2 - nível 2 = 4 - nível 3 = 8 ... nível 10 = 1024
     
             // FÓRMULAS:
     
                 // elementos por nível = 2^n
-                // elementos 2 ^n+1 -1
-                // altura de uma árvore = n+1
+                // elementos 2 ^n+1 -1 OU 2 elevado a altura (n+1) -1
+                // altura de uma árvore = n+1 
+                    // ex: se a altura de uma árvore é 3, o nível máximo da árvore é 2 (pq a contagem começa do 0)
     
     
         // ÁRVORE BINÁRIA COMPLETA = Uma árvore cheia em que todos os nós folhas não estão no mesmo nível, MAS se ela estiver incompleta no último nível, todos os nós folhas devem estar no lado esquerdo
