b39d368044c16c23d2e602e785f50367682a7bc2
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-15 15:05:28 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
@@ -11,7 +11,11 @@ public class Arvore { // Árvore
     // NÓ AVÔ = Nó que possui um nó filho e esse filho possui outros nós filhos
     // NÓS IRMÃOS = nós que possuem o mesmo pai
     // FOLHAS = Nós que não possuem filhos
-    
-    
+    // ÁRVORES BINÁRIAS = São árvores em que um nó só pode possuir no máximo 2 filhos
+        // CHEIA = Quando cada nó possui 2 ou 0 filhos
+        // ÁRVORE NÃO CHEIA = nó possui 1 filho
+        // ÁRVORE PERFEITA = Uma árvore cheia onde todas as FOLHAS estão no mesmo nível - em cada nível = 2^n 
+            // nível 1 = 1 - nível 2 = 2 - nível 3 = 4 - nível 4 = 8 ...
+        // ÁRVORE BINÁRIA COMPLETA = Uma árvore cheia em que todos os nós folhas não estão no mesmo nível, MAS se ela estiver incompleta no último nível, todos os nós folhas devem estar no lado esquerdo
     
 }
