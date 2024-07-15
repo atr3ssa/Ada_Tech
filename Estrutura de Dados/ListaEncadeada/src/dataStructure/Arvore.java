f123eaf930ed4ba1e32466e2d7045c35e429b080
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-15 13:57:53 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Arvore.java
@@ -3,5 +3,15 @@ package dataStructure;
 
 
 public class Arvore { // Árvore
+    // Arvore = Estrutura de dados não linear / Semelhante a uma árvore genealógica
+    // Em cada arvore, cada nó pode ser ligado com vários outros nós
+    // RAIZ = nó que não possui pai
+    // NÓ PAI = nó que possui filhos/ links ou nós ligados
+    // NÓ FILHO = nó que possui um nó pai
+    // NÓ AVÔ = Nó que possui um nó filho e esse filho possui outros nós filhos
+    // NÓS IRMÃOS = nós que possuem o mesmo pai
+    // FOLHAS = Nós que não possuem filhos
+    
+    
     
 }
