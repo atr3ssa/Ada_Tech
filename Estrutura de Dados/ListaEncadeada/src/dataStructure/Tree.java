2a0ed2699ad8cb72cce582b3e9ce6762bd9b2309
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-23 18:07:27 -0300

    ada

--- Estrutura de Dados/ListaEncadeada/src/dataStructure/Tree.java
+++ Estrutura de Dados/ListaEncadeada/src/dataStructure/Tree.java
@@ -70,31 +70,47 @@ public class Tree { // Árvore
         }
     }
     
-    public void insert(int value){
-        if (root == null) root = new Node(value);
-        else{
-            Node newNode = new Node(value);
-            Queue<Node> queue = new LinkedList<>();
-            queue.add(root);
-            while(queue.size()>0) {
-                Node currentNode = queue.remove();
-                if(currentNode.left == null){
-                    currentNode.left = newNode;
+    public void insert(int value){ // método inserir (recebe valor p inserir na arvore)
+        if (root == null) root = new Node(value); // se a raiz estiver vazia, será inicializada a arvore / raiz = novo nó
+        else{ //senão usarei uma fila
+            Node newNode = new Node(value); // novo nó
+            Queue<Node> queue = new LinkedList<>(); // implementação será uma lista ligada
+            queue.add(root); // add na fila o nó raiz
+            while(queue.size()>0) { // enquanto minha fila tiver elementos
+                Node currentNode = queue.remove(); // vou ler o elemento da fila e retirar da fila
+                if(currentNode.left == null){ // se o elemento corrente, o lado esquerdo estiver vazio
+                    currentNode.left = newNode; // será add um novo elemento na posição esquerda
                     break;
-                } else{
-                    queue.add(currentNode.left);
+                } else{//senão
+                    queue.add(currentNode.left); // vou add na fila aquele lado esquerdo
                 }
-                if (currentNode.right == null) {
-                    currentNode.right = newNode;
-                } else{
-                    queue.add(currentNode.right);
+                if (currentNode.right == null) { // o elemento, o lado direito estiver vazio
+                    currentNode.right = newNode; // será add um novo elemento na posição direita
+                    break;
+                } else{ //senão
+                    queue.add(currentNode.right); // vou add na fila aquele lado direito
                 }
             }
         }
     }
     
     
-    
+    public static void main(String[] args) {
+        Tree tree = new Tree(); // criei minha arvore, que a principio está vazia
+        
+        //inserindo na arvore
+        tree.insert(1); 
+        tree.insert(2);
+        tree.insert(3);
+        
+        System.out.println(tree.root.value); // mandar imprimir o valor da raiz da arvore
+        System.out.println(tree.root.left.value); // mandar imprimir o valor do lado esquerdo da raiz da arvore
+        System.out.println(tree.root.right.value); // mandar imprimir o valor do lado direito da raiz da arvore
+        
+        System.out.println(tree.root.isLeaf()); // retornar se a raiz é folha (true or false)
+        System.out.println(tree.root.left.isLeaf()); // retornar se a esquerda da raiz é folha
+        System.out.println(tree.root.right.isLeaf()); // retornar se a direita da raiz é folha
+    }
    
     
     
