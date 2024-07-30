10ad208d742d5e54108084d11d9ea9650b7cdb37
Andressa Rabêlo <dre.emy89@gmail.com>
2024-07-30 18:56:57 -0300

    ada

---
+++ Estrutura de Dados/ListaEncadeada/src/problem/BalanceamentoDeParenteses.java
@@ -0,0 +1,21 @@
+
+package problem;
+
+
+public class BalanceamentoDeParenteses {
+    
+    /* PROBLEMA: BALANCEAMENTO DE COLCHETES EM UMA EXPRESSÃO
+            Escreva um programa para examinar se os pares e as ordem estçao corretos na expressão dada.
+ 
+        - Classico;
+        - Envolve pilhas;
+    
+        Solução: uo de pilhas
+        1- Empilhar caracteres de abertura;
+        2- Ao identificar caracter de fechamento:
+            - desempilhar e verificar se coincide com abertura;
+        3- Ao final a pilha deve estar vazia.
+
+*/
+    
+}
