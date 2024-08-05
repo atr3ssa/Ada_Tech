9be5add141e6abf310b18b76c4dce6bf58686057
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-05 19:04:26 -0300

    ada

--- Estrutura de Dados/Estrutura de Dados/src/problem/p.java
+++ Estrutura de Dados/Estrutura de Dados/src/problem/p.java
@@ -3,5 +3,17 @@ package problem;
 
 
 public class p {
+    /* PROBLEMA: Revertendo os primeiros K elementos de uma fila
+        Escreva um p
+            
+            - Uso uma fila = queue;
+            - 1: P os primeiros k elementos:
+                - Desenfileirar elemento x;
+                - Reordena K-1 elementos;
+                - Enfileirar elemento X;
+            - P os N-K elementos:
+                - desenfileirar
+                - enfileirar
+    */
     
 }
