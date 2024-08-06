599815bee6b8b5973d96e7e7eae9d2a4ef6e5ed3
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-06 16:53:09 -0300

    ada

--- Estrutura de Dados/Estrutura de Dados/src/problem/n_.java
+++ Estrutura de Dados/Estrutura de Dados/src/problem/n_.java
@@ -8,6 +8,12 @@ public class n_ {
     
             Dada uma árvore binária, encontre o maior valor em cada nível
     
+    ALGORITMO:
+        1- Utilize o algoritmo BFS para percorrer a árvore e uma lista auxiliar
+        2- Para cada elemento e seu nível da árvore:
+            - Se a lista possuir o índice = nível, substitua pelo maior valor;
+            - Senão adicione na lista
+    
     
     
     */
