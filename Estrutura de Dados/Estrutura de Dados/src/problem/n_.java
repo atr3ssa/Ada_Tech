75497c4d7a8bbc61155b717d1675cd96ecd70794
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-06 16:49:15 -0300

    ada

---
+++ Estrutura de Dados/Estrutura de Dados/src/problem/n_.java
@@ -0,0 +1,15 @@
+// pacote
+package problem;
+
+
+public class n_ {
+    
+    /* PROBLEMA: Maior valor em cada nível da Árvore Binária
+    
+            Dada uma árvore binária, encontre o maior valor em cada nível
+    
+    
+    
+    */
+    
+}
