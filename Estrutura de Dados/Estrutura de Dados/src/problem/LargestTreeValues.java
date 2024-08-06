2e831833e9cbd87660ed8e6015731ab49a8ab74d
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-06 17:38:19 -0300

    ada

--- Estrutura de Dados/Estrutura de Dados/src/problem/LargestTreeValues.java
+++ Estrutura de Dados/Estrutura de Dados/src/problem/LargestTreeValues.java
@@ -13,7 +13,7 @@ public class LargestTreeValues {
             Dada uma árvore binária, encontre o maior valor em cada nível
     
     ALGORITMO:
-        1- Utilize o algoritmo BFS para percorrer a árvore e uma lista auxiliar
+        1- Utilize o algoritmo BFS (não é necessário ser um BFS) para percorrer a árvore e uma lista auxiliar
         2- Para cada elemento e seu nível da árvore:
             - Se a lista possuir o índice = nível, substitua pelo maior valor;
             - Senão adicione na lista/ vetor
