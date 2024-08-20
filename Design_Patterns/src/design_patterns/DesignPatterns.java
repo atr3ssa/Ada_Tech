beab95dfe384502c6753e7ef51186421b67bf062
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-20 18:06:39 -0300

    ada

---
+++ Design_Patterns/src/design_patterns/DesignPatterns.java
@@ -0,0 +1,28 @@
+
+package design_patterns;
+
+
+public class DesignPatterns {
+   /*
+    Design patterns:
+ Padrões utilizados para solucionar um problema
+
+existem 23 padrões catalogados, sendo eles divididos em:
+
+- Creational (Criacional): trata da criação do objetos
+	cuida de como os objetos são instaciados, criados, se serão varias instancias na memoria ou só uma
+	
+
+- Structural (Estrutural): relação dos objetos e como eles interagem entre si (Composição por exemplo)
+	Ex: classe pessoa que tem uma classe endereço que faz parte dela
+
+-Behavioral (Comportamental): Comunicação entre os objetos, em termos de responsabilidades e algoritmos. Chamadas de métodos.
+	
+	Ex: O que o objetos pessoa vai fazer e qual a responsabilidade dele, o que o objeto endereço vai fazer, como fazer...
+
+
+recomendação: Design Patterns : elements Reusable Object-Oriented Software
+
+    
+    */ 
+}
