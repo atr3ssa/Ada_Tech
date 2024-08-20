fd91e9518bfaf73baa2076cd9d170bfe88d300b0
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-20 18:17:20 -0300

    ada

--- Design_Patterns/src/builder/problem/Solution/Pessoa.java
+++ Design_Patterns/src/builder/problem/Solution/Pessoa.java
@@ -4,7 +4,8 @@ package builder.problem.Solution;
 // Classe pessoa com os atributos: Nome, sobrenome, documento, email, apelido, data de nascimento
 public class Pessoa { 
     // uma estrutura da qual posso criar objetos concretos
-            //  Uma classe é uma planta e as casas criadas a partir dessa planta, são os objetos
+            //  Uma classe é uma planta e as casas criadas a partir dessa planta, são os objetos (criados a partir dos construtores
+    
     // encanpsular  atributos;
    private String nome; 
    private String sobrenome;
@@ -16,6 +17,7 @@ public class Pessoa {
    // Inserir constructor
    
     public Pessoa(String nome, String sobrenome, String documento, String email, String apelido, String dataNascimento) {
+        // Quando for criado uma pessoa, terei que passar todos esses atributos
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.documento = documento;
@@ -23,6 +25,58 @@ public class Pessoa {
         this.apelido = apelido;
         this.dataNascimento = dataNascimento;
     }
+    
+    // getters e Setters 
+        // p setar e obter os valores
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
    
    
 }
