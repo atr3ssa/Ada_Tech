4cfe6a1e264b3e951d8bf6cc951cb927874cb7be
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-20 18:13:53 -0300

    ada

--- Design_Patterns/src/builder/problem/Solution/Pessoa.java
+++ Design_Patterns/src/builder/problem/Solution/Pessoa.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// pacote
 package builder.problem.Solution;
 
-/**
- *
- * @author DreEm
- */
-public class Pessoa {
-    
+// Classe pessoa com os atributos: Nome, sobrenome, documento, email, apelido, data de nascimento
+public class Pessoa { 
+    // uma estrutura da qual posso criar objetos concretos
+            //  Uma classe é uma planta e as casas criadas a partir dessa planta, são os objetos
+    // encanpsular  atributos;
+   private String nome; 
+   private String sobrenome;
+   private String documento;
+   private String email;
+   private String apelido;
+   private String dataNascimento; // poderia ser um local date
+
+   // Inserir constructor
+   
+    public Pessoa(String nome, String sobrenome, String documento, String email, String apelido, String dataNascimento) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.documento = documento;
+        this.email = email;
+        this.apelido = apelido;
+        this.dataNascimento = dataNascimento;
+    }
+   
+   
 }
