bc763171829745a5211b006fec0b154eb6043494
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-21 19:26:25 -0300

    ada

--- Design_Patterns/src/builder/problem/Solution/PessoaBuilder.java
+++ Design_Patterns/src/builder/problem/Solution/PessoaBuilder.java
@@ -1,13 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// pacote
 package builder.problem.Solution;
 
-/**
- *
- * @author DreEm
- */
+import java.time.LocalDate;
+
+
 public class PessoaBuilder {
+    // uma estrutura da qual posso criar objetos concretos
+            //  Uma classe é uma planta e as casas criadas a partir dessa planta, são os objetos (criados a partir dos construtores
     
+    // encanpsular  atributos;
+   private String nome; 
+   private String sobrenome;
+   private String documento;
+   private String email;
+   private String apelido;
+   private LocalDate dataNascimento; // poderia ser um localDate
 }
