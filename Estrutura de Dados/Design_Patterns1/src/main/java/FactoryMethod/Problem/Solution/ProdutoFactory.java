8de7ceae60b635f2d4793a035c951100bb1d2645
Andressa Rabêlo <dre.emy89@gmail.com>
2024-08-22 19:05:33 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/FactoryMethod/Problem/Solution/ProdutoFactory.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/FactoryMethod/Problem/Solution/ProdutoFactory.java
@@ -2,6 +2,8 @@
 package FactoryMethod.Problem.Solution;
 
 import FactoryMethod.Problem.Produto;
+import FactoryMethod.Problem.ProdutoDigital;
+import FactoryMethod.Problem.ProdutoFisico;
 import FactoryMethod.Problem.TipoDeProdutoEnum;
 
 
@@ -9,7 +11,26 @@ import FactoryMethod.Problem.TipoDeProdutoEnum;
 
 public class ProdutoFactory {
     
-    public static Produto getInstance(TipoDeProdutoEnum tipoDeProdutoEnum){
+    public static Produto getInstance(TipoDeProdutoEnum tipoDeProdutoEnum){ // recebo como parametro
+        
+        switch(tipoDeProdutoEnum){ // Usando o Switchcase - Caso o tipo de produto seja
+           
+            case FISICO: // fisico
+                ProdutoFisico produtoFisico = new ProdutoFisico(); // criar o produto
+                produtoFisico.setPossuiDimensoesFisicas(true); // informar que ele terá dimensões fisicas
+                return produtoFisico; // retornar o produto
+                
+            case DIGITAL:
+                ProdutoDigital produtoDigital = new ProdutoDigital(); // criar o produto
+                produtoDigital.setPossuiDimensoesFisicas(false); // informar que ele terá dimensões fisicas
+                return produtoDigital; // retornar o produto
+                
+            default:
+                
+                
+        }
+                
+                
         
     
 }
