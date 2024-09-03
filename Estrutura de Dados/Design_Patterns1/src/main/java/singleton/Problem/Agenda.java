d8fab95997f64e0243084678ad198cbc39ebb92b
Andressa Rabêlo <dre.emy89@gmail.com>
2024-09-03 18:45:21 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/singleton/Problem/Agenda.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/singleton/Problem/Agenda.java
@@ -1,7 +1,12 @@
 // pacote
 package singleton.Problem;
 
+import java.util.HashMap; // import hashMap
+import java.util.Map; // Map
+
 
 public class Agenda {
+    // hashmap de String
     
+    private Map<String, Boolean> diasDisponiveis = new HashMap<>();
 }
