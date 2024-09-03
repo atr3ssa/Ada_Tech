45ceb404fb6f39a8d045e5e352c97934bf02209d
Andressa Rabêlo <dre.emy89@gmail.com>
2024-09-03 18:55:48 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/singleton/Problem/Agenda.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/singleton/Problem/Agenda.java
@@ -8,10 +8,15 @@ import java.util.Map; // Map
 public class Agenda {
     // hashmap de String
     
-    private Map<String, Boolean> diasDisponiveis = new HashMap<>();
+    private Map<String, Boolean> diasDisponiveis = new HashMap<>(); // HashMap de String e boolean
     
-    public Agenda(){
+    public Agenda(){ // Construtor
+        
+        // mostro os dias disponiveis
         diasDisponiveis.put("Domingo", Boolean.TRUE);
         
     }
+    public Map<String, Boolean> diasDisponiveis(){return diasDisponiveis;}
+    
+    public void ocupa(String dia){ diasDisponiveis.replace(dia, Boolean.FALSE);}
 }
