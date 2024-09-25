b8f821d76f8fe067af796e4fa0af1510c13e7611
Andressa Rabêlo <dre.emy89@gmail.com>
2024-09-25 19:10:40 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/prototype/TipoBordaEnum.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/prototype/TipoBordaEnum.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// pacote
 package prototype;
 
-/**
- *
- * @author DreEm
- */
-public class TipoBordaEnum {
+
+public enum TipoBordaEnum {
+    
+    FINA, GROSSA, TRACEJADA;
     
 }
