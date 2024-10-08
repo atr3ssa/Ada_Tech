8c3bcef375048444111187247b276a862ff8c158
Andressa Rabêlo <dre.emy89@gmail.com>
2024-10-08 17:28:22 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/prototype/solution/BotaoRegistry.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/prototype/solution/BotaoRegistry.java
@@ -1,11 +1,29 @@
 // pacote
 package prototype.solution;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import prototype.Botao;
 
 public class BotaoRegistry { // aplicar Singleton
     
     private static BotaoRegistry botaoRegistry;
+    
+    //criar um Map
+    private static Map<String, Botao> REGISTRY = new HashMap<>(); // Map, o cara que vai registrar o nosso protótipo
+    
+    static {
+        // criar os botões e inicializar:
+        Botao botao_amarelo = new Botao();
+        Botao botao_vermelho = new Botao();
+        Botao botao_azul = new Botao();
+        
+        // add botões:
+        REGISTRY.put("BOTAO_AMARELO", botao_amarelo);
+        REGISTRY.put("BOTAO_VERMELHO", botao_vermelho);
+        REGISTRY.put("BOTAO_AZUL", botao_azul);
+    }//bloco estatico
     
     public static BotaoRegistry getInstance(){
       if(Objects.isNull(botaoRegistry))  {
@@ -14,5 +32,6 @@ public class BotaoRegistry { // aplicar Singleton
       }
       return botaoRegistry;
     }
+    // A partir da inicialização da Classe:
     
 }
