ca972d738859b481391fd9e8f4934c4685d0a450
Andressa Rabêlo <dre.emy89@gmail.com>
2024-10-08 17:20:30 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/prototype/solution/BotaoRegistry.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/prototype/solution/BotaoRegistry.java
@@ -1,7 +1,18 @@
 // pacote
 package prototype.solution;
 
+import java.util.Objects;
 
 public class BotaoRegistry { // aplicar Singleton
+    
+    private static BotaoRegistry botaoRegistry;
+    
+    public static BotaoRegistry getInstance(){
+      if(Objects.isNull(botaoRegistry))  {
+          botaoRegistry = new BotaoRegistry();
+          return botaoRegistry;
+      }
+      return botaoRegistry;
+    }
     
 }
