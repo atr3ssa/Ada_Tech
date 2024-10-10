49f3a057e8a8216d0b3dfe3968efc065f75c7ea6
Andressa Rabêlo <dre.emy89@gmail.com>
2024-10-10 19:13:36 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/proxy/PessoaService.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/proxy/PessoaService.java
@@ -1,7 +1,24 @@
-// pacote
 package proxy;
 
 
-public class PessoaService {
+public class PessoaService { // minha classe de serviço
+
+
+    private PessoaRepository pessoaRepository; // atributo / composição
+
+// métodos:
     
+    public PessoaService(PessoaRepository pessoaRepository) { // o constructor fará uma engessão de dependências aqui (pesquisar)
+// Inversão de controle (não é necessário criar um objeto, "alguém" cria e injeta
+
+        this.pessoaRepository = pessoaRepository;
+
+}
+    public void save(Pessoa pessoa){ // vai salvar 
+        pessoaRepository.save(pessoa); // e passar p/ a pessoaRepository a responsablidade
+}
+     public Pessoa findById(Long id){ // vai receber um id por parâmetro e retornar pessoa de acordo com o id
+        return pessoaRepository.findById(id);
+    }
+
 }
