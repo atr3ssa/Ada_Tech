e2c755e29213b77269cbb2a491e79e5da1f931e9
Andressa Rabêlo <dre.emy89@gmail.com>
2024-10-16 19:26:24 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/proxy/PessoaService.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/proxy/PessoaService.java
@@ -1,26 +1,27 @@
 package proxy;
 
 import builder.problem.Solution.Pessoa;
+import proxy.Solution.PessoaRepositoryProxy;
 
 
 public class PessoaService { // minha classe de serviço
 
 
-    private PessoaRepository pessoaRepository; // atributo / composição
+    private PessoaRepositoryProxy pessoaRepositoryProxy; // atributo / composição
 
 // métodos:
     
-    public PessoaService(PessoaRepository pessoaRepository) { // o constructor fará uma engessão de dependências aqui (pesquisar)
+    public PessoaService(PessoaRepositoryProxy pessoaRepositoryProxy) { // o constructor fará uma engessão de dependências aqui (pesquisar)
 // Inversão de controle (não é necessário criar um objeto, "alguém" cria e injeta
 
-        this.pessoaRepository = pessoaRepository;
+        this.pessoaRepositoryProxy = pessoaRepositoryProxy;
 
 }
     public void save(Pessoa pessoa){ // vai salvar 
-        pessoaRepository.save(pessoa); // e passar p/ a pessoaRepository a responsablidade
+        pessoaRepositoryProxy.save(pessoa); // e passar p/ a pessoaRepository a responsablidade
 }
      public Pessoa findById(Long id){ // vai receber um id por parâmetro e retornar pessoa de acordo com o id
-        return pessoaRepository.findById(id);
+        return pessoaRepositoryProxy.findById(id);
     }
        
     }
--- Estrutura de Dados/Design_Patterns1/src/main/java/proxy/Solution/PessoaTestProxy.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/proxy/Solution/PessoaTestProxy.java
@@ -1,6 +1,30 @@
 // pacote
 package proxy.Solution;
 
+import builder.problem.Solution.Pessoa;
+import java.time.LocalDate;
+import proxy.PessoaService;
+
 public class PessoaTestProxy {
-    
+     public static void main(String[] args){
+        PessoaService pessoaService = new PessoaService(new PessoaRepositoryProxy()); // injetando a dependencia
+        
+        Pessoa pessoa = new Pessoa.PessoaBuilder()
+                .sobrenome("Rabelo")
+                .dataNascimento(LocalDate.of(2001,03,06))
+                .nome("Andressa")
+                .apelido("Dressa")
+                .documento("12312312312")
+                .build();
+        
+        pessoaService.save(pessoa);
+        
+        Pessoa pessoaRetornada = pessoaService.findById(1L);
+        System.out.println(pessoaRetornada);
+        
+        // Proxy: Quero por log, mas n´posso alterar a classe (PessoaRepository)
+        // Service vai chamar o proxy que vai chamar o pessoaRepository
+        
+        
+    }
 }
