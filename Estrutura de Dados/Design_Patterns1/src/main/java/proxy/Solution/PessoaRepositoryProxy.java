cc51fb9048f2c1542dbe30e11b634e03922186b1
Andressa Rabêlo <dre.emy89@gmail.com>
2024-10-16 19:47:00 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/proxy/Solution/PessoaRepositoryProxy.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/proxy/Solution/PessoaRepositoryProxy.java
@@ -4,6 +4,7 @@ package proxy.Solution;
 import builder.problem.Solution.Pessoa;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Logger;
 import proxy.PessoaRepository;
 
@@ -24,6 +25,14 @@ public class PessoaRepositoryProxy extends PessoaRepository{
     @Override
     public Pessoa findById(Long Id){
         log.info("Iniciando chamada do método findById...");
+        if(Objects.nonNull(cache.get(Id))){
+        log.info("Está pegando do cache...");  
+        return cache.get(Id);
+        } else{
+          log.info("Está pegando de PessoaRepository..."); 
+          Pessoa pessoa = super.findById(Id);
+          cache.put(Id,pessoa);
+        }
         Pessoa pessoa = super.findById(Id);
         log.info("Chamada do método findById finalizada");
         return pessoa;
