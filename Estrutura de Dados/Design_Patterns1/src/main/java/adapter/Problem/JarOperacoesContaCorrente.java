3a1ac7450a2f4f8cb3aaa080fbd4d8847f1d7275
Andressa Rabêlo <dre.emy89@gmail.com>
2024-12-09 18:33:07 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/JarOperacoesContaCorrente.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/JarOperacoesContaCorrente.java
@@ -3,17 +3,22 @@ package adapter.Problem;
 
 import java.math.BigDecimal;
 
-
+// imagina que vc faz parte de uma parte de financero do banco
 public class JarOperacoesContaCorrente {
     // para simular um JAR
     
-    public boolean validaSaldo(BigDecimal valorPretendidoSangue){
+    // métodos (do seu Jar)
+    public boolean validaSaldo(BigDecimal valorPretendidoSangue){ // validar o saldo
         System.out.println("validando o saldo...");
         return true;
     }
     
-    public void saca(BigDecimal valorPretendido){
+    public void saca(BigDecimal valorPretendido){ // sacar o valor da conta
         System.out.println("Sacando...");
+    }
+    
+    public void deposita(BigDecimal valor){ // depoistar o valor da conta
+        System.out.println("Depositando...");
     }
     
 }
