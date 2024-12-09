2aaa9a7d724866534d20451c279770a9b8ac3ded
Andressa Rabêlo <dre.emy89@gmail.com>
2024-12-09 20:14:15 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/Solution/ClientJarOperacoesContaCorrenteAdapter.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/Solution/ClientJarOperacoesContaCorrenteAdapter.java
@@ -1,7 +1,25 @@
 // pacote
 package adapter.Problem.Solution;
 
+import adapter.Problem.JarOperacoesContaCorrente;
+import java.math.BigDecimal;
+
 
 public class ClientJarOperacoesContaCorrenteAdapter {
+    private JarOperacoesContaCorrenteAdapter jarContaAdapter;
     
+    public ClientJarOperacoesContaCorrenteAdapter(JarOperacoesContaCorrenteAdapter jarContaadapter){
+        this.jarContaAdapter=jarContaAdapter; // constructor (está recebendo o jar)
+    }
+    
+    
+    // realizar as operações:
+   
+    public void saca(BigDecimal valorPretendido){
+        jarContaAdapter.saca(valorPretendido);// sacar, a partir do jar
+    }
+    
+    public void deposita(BigDecimal valor){
+        jarContaAdapter.deposita(valor); // depositar, a partir do jar
+    }
 }
