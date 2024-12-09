9d05c8335f086e73269183fb85ac138139ff1685
Andressa Rabêlo <dre.emy89@gmail.com>
2024-12-09 18:49:08 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/ClientJarOperacoesContaCorrente.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/ClientJarOperacoesContaCorrente.java
@@ -3,21 +3,28 @@ package adapter.Problem;
 
 import java.math.BigDecimal;
 
-
-public class ClientJarOperacoesContaCorrente {
+// biblioteca fechada que não podemos mexer nela
+// esse client vai usar o Jar
+public class ClientJarOperacoesContaCorrente { 
     
     private JarOperacoesContaCorrente jocc;
     
     public ClientJarOperacoesContaCorrente(JarOperacoesContaCorrente jocc){
-        this.jocc=jocc;
+        this.jocc=jocc; // constructor (está recebendo o jar)
     }
     
+    
+    // realizar as operações:
+    
     public boolean validaSaldo(BigDecimal valorPretendidoSaque){
-        return jocc.validaSaldo(valorPretendidoSaque);
+        return jocc.validaSaldo(valorPretendidoSaque); // validar, a partir do jar
     }
     
     public void saca(BigDecimal valorPretendido){
-        jocc.saca(valorPretendido);
+        jocc.saca(valorPretendido);// sacar, a partir do jar
     }
     
+    public void deposita(BigDecimal valor){
+        jocc.deposita(valor); // depositar, a partir do jar
+    }
 }
