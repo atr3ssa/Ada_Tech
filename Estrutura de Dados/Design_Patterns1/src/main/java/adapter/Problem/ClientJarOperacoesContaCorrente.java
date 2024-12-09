b6d28d965e3fdcf3b044292081a34c25ac27725c
Andressa Rabêlo <dre.emy89@gmail.com>
2024-12-09 18:43:21 -0300

    ada

---
+++ Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/ClientJarOperacoesContaCorrente.java
@@ -0,0 +1,23 @@
+// pacote
+package adapter.Problem;
+
+import java.math.BigDecimal;
+
+
+public class ClientJarOperacoesContaCorrente {
+    
+    private JarOperacoesContaCorrente jocc;
+    
+    public ClientJarOperacoesContaCorrente(JarOperacoesContaCorrente jocc){
+        this.jocc=jocc;
+    }
+    
+    public boolean validaSaldo(BigDecimal valorPretendidoSaque){
+        return jocc.validaSaldo(valorPretendidoSaque);
+    }
+    
+    public void saca(BigDecimal valorPretendido){
+        jocc.saca(valorPretendido);
+    }
+    
+}
