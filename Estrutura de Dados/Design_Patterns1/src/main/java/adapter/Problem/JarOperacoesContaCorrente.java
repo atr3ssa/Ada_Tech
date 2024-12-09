76413b3e482265d03efaacc32731ef089b05e106
Andressa Rabêlo <dre.emy89@gmail.com>
2024-12-09 18:29:53 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/JarOperacoesContaCorrente.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/adapter/Problem/JarOperacoesContaCorrente.java
@@ -1,7 +1,19 @@
 // pacote
 package adapter.Problem;
 
+import java.math.BigDecimal;
+
 
 public class JarOperacoesContaCorrente {
+    // para simular um JAR
+    
+    public boolean validaSaldo(BigDecimal valorPretendidoSangue){
+        System.out.println("validando o saldo...");
+        return true;
+    }
+    
+    public void saca(BigDecimal valorPretendido){
+        System.out.println("Sacando...");
+    }
     
 }
