836dfff330d1ff087b5d210d00d2b246e5c6a839
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-24 19:36:18 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Funcionario.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Funcionario.java
@@ -8,6 +8,30 @@ public class Funcionario {
     private String nome;
     private BigDecimal salario; // mais preciso para tratar moedas
     private TipoContratacaoEnum tipoContratacao;
+
+    public String getNome() {return nome;}
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public TipoContratacaoEnum getTipoContratacao() {
+        return tipoContratacao;
+    }
+
+    public void setTipoContratacao(TipoContratacaoEnum tipoContratacao) {
+        this.tipoContratacao = tipoContratacao;
+    }
+    
+    
     
     
     
--- Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Problem/TesteAjusteSalarioSemStrategy.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Problem/TesteAjusteSalarioSemStrategy.java
@@ -1,7 +1,38 @@
 // pacote
 package strategy.Problem;
 
+import java.math.BigDecimal;
+import strategy.Funcionario;
+import strategy.TipoContratacaoEnum;
+
 
 public class TesteAjusteSalarioSemStrategy {
+    public static void main(String[] args) {
+        ReajusteAnualSalario reajusteAnualSalario = new ReajusteAnualSalario();
+        Funcionario funcionarioCLT = new Funcionario();
+        funcionarioCLT.setNome("Victor");
+        funcionarioCLT.setTipoContratacao(TipoContratacaoEnum.CLT);
+        funcionarioCLT.setSalario(new BigDecimal(5000));
+        
+        Funcionario funcionarioPJ = new Funcionario();
+        funcionarioPJ.setNome("Podowski");
+        funcionarioPJ.setTipoContratacao(TipoContratacaoEnum.PJ);
+        funcionarioPJ.setSalario(new BigDecimal(10000));
+        
+        Funcionario funcionarioEstagio = new Funcionario();
+        funcionarioEstagio.setNome("Osvaldo");
+        funcionarioEstagio.setTipoContratacao(TipoContratacaoEnum.ESTAGIO);
+        funcionarioEstagio.setSalario(new BigDecimal(2000));
+        
+        //Calcular nossos reajustes aqui
+        
+        System.out.println(funcionarioCLT);
+        System.out.println(funcionarioPJ);
+        System.out.println(funcionarioEstagio);
+                
+                
+                
+        
+    }
     
 }
