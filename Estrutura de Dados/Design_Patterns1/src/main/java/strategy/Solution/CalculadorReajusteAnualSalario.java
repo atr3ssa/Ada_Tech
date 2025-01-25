4bc55ced52b52ce0c4a5f17cc3176dab8b061ed2
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-24 21:39:29 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Solution/CalculadorReajusteAnualSalario.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/strategy/Solution/CalculadorReajusteAnualSalario.java
@@ -4,9 +4,14 @@ package strategy.Solution;
 import strategy.Funcionario;
 
 
-public interface CalculadorReajusteAnualSalario {
+public interface CalculadorReajusteAnualSalario { // ganhamos polimorfismo aqui
     // definir calculadores
     
     void calcularReajusteAnual(Funcionario funcionario);
     
 }
+
+// vantagem: se amanhã surgr um novo calculo, n preciso alterar nada (por if a mais), só preciso criar um cra novo e implementar
+
+// estudar sobre solid (principio o)
+
