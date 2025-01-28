edade34182cc6c27567ba00ecf14affbce809810
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-28 20:19:42 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroFIAT.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroFIAT.java
@@ -1,7 +1,25 @@
 // pacote
 package chainofresponsibility.Solution;
 
+import chainofresponsibility.Carro;
+import chainofresponsibility.Marca;
+import java.math.BigDecimal;
 
-public class DescontoCarroFIAT {
+
+public class DescontoCarroFIAT extends DescontoCarro {
+
+    public DescontoCarroFIAT(DescontoCarro proximoDesconto) {
+        super(proximoDesconto); // chamar o constructor da classe pai
+    }
+
+    @Override
+    public BigDecimal aplicaDesconto(Carro carro) {
+        BigDecimal valorVenda = carro.getPreco();
+        
+        if (Marca.FIAT.equals(carro.getMarca())){ // se o carro for da fiar, ganha um desconto de 1000
+         return valorVenda.subtract(new BigDecimal(1000)); // através do método subtract (subtrair) 
+       }
+        return proximoDesconto.aplicaDesconto(carro);
+    }
     
 }
