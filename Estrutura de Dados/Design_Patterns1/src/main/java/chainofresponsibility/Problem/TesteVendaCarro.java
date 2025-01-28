ad17bb429dd857ef125c0abbf1d6b19e1786b988
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-28 19:32:47 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Problem/TesteVendaCarro.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Problem/TesteVendaCarro.java
@@ -1,7 +1,35 @@
 // Pacote
 package chainofresponsibility.Problem;
 
+import chainofresponsibility.Carro;
+import chainofresponsibility.Marca;
+import java.math.BigDecimal;
+
 
 public class TesteVendaCarro {
     
+    public static void main(String[] args) {
+        VendaCarroService vendaCarroService = new VendaCarroService();
+        
+        Carro carro1 = new Carro();
+        carro1.setMarca(Marca.FORD);
+        carro1.setModelo("Fusion");
+        carro1.setPreco(new BigDecimal(90000.00));
+        
+        BigDecimal valorVenda1 = vendaCarroService.calculaValorVenda(carro1);
+        System.out.println("Valor de venda Fusion: " + carro1.getPreco());
+        System.out.println("Valor de venda Fusion com desconto: " + valorVenda1);
+        
+        Carro carro2= new Carro();
+        carro2.setMarca(Marca.FIAT);
+        carro2.setModelo("UNO");
+        carro2.setPreco(new BigDecimal(40000.00));
+        
+        BigDecimal valorVenda2 = vendaCarroService.calculaValorVenda(carro2);
+        System.out.println("Valor de venda UNO: " + carro1.getPreco());
+        System.out.println("Valor de venda UNO com desconto: " + valorVenda2);
+        
+        
+        
+    }
 }
--- Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Problem/VendaCarroService.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Problem/VendaCarroService.java
@@ -6,20 +6,22 @@ import chainofresponsibility.Marca;
 import java.math.BigDecimal;
 
 
-public class VendaCarroService {
+public class VendaCarroService { // calcula o valor da venda - aplicar descontos
     public BigDecimal calculaValorVenda(Carro carro) {
-       BigDecimal valorVenda = carro.getPreco();
+       BigDecimal valorVenda = carro.getPreco(); // pega o valor da venda
        
-       if (Marca.FIAT.equals(carro.getMarca())){
-         return valorVenda.subtract(new BigDecimal(1000));  
+       // BigDecimal (método)
+       
+       if (Marca.FIAT.equals(carro.getMarca())){ // se o carro for da fiar, ganha um desconto de 1000
+         return valorVenda.subtract(new BigDecimal(1000)); // através do método subtract (subtrair) 
        }
        
        if (Marca.FORD.equals(carro.getMarca())){
            return valorVenda.subtract(new BigDecimal(2000));
        }
        
-       if (carro.getPreco().compareTo(new BigDecimal(100000.00)) > 0){
-           return valorVenda.subtract(new BigDecimal(10000));
+       if (carro.getPreco().compareTo(new BigDecimal(100000.00)) > 0){ // se o preço de venda maior que 100.000, ganha um desconto de 10.000
+           return valorVenda.subtract(new BigDecimal(10000)); // compare to: comparar
        }
     }
     
