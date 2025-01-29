fde838e89c99fabbf9aa14618f57f989c81abaf5
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-28 21:38:46 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroValorMaiorQueCemMil.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroValorMaiorQueCemMil.java
@@ -1,7 +1,24 @@
 // pacote
 package chainofresponsibility.Solution;
 
+import chainofresponsibility.Carro;
+import java.math.BigDecimal;
 
-public class DescontoCarroValorMaiorQueCemMil {
+
+public class DescontoCarroValorMaiorQueCemMil extends DescontoCarro {
+
+    public DescontoCarroValorMaiorQueCemMil(DescontoCarro proximoDesconto) {
+        super(proximoDesconto);
+    }
+
+    @Override
+    public BigDecimal aplicaDesconto(Carro carro) {
+        BigDecimal valorVenda = carro.getPreco();
+        
+        if (carro.getPreco().compareTo(new BigDecimal(100000.00)) > 0){ // se o preço de venda maior que 100.000, ganha um desconto de 10.000
+           return valorVenda.subtract(new BigDecimal(10000)); // compare to: comparar
+       }
+         return proximoDesconto.aplicaDesconto(carro);
+    }
     
 }
