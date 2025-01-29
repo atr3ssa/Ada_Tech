2ea1b3ca58a4a381b491345fb381a8835be80032
Andressa Rabêlo <dre.emy89@gmail.com>
2025-01-28 21:34:40 -0300

    ada

--- Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroFORD.java
+++ Estrutura de Dados/Design_Patterns1/src/main/java/chainofresponsibility/Solution/DescontoCarroFORD.java
@@ -1,7 +1,25 @@
 // pacote
 package chainofresponsibility.Solution;
 
+import chainofresponsibility.Carro;
+import chainofresponsibility.Marca;
+import java.math.BigDecimal;
 
-public class DescontoCarroFORD {
+
+public class DescontoCarroFORD extends DescontoCarro{
+
+    public DescontoCarroFORD(DescontoCarro proximoDesconto) {
+        super(proximoDesconto);
+    }
+
+    @Override
+    public BigDecimal aplicaDesconto(Carro carro) {
+        BigDecimal valorVenda = carro.getPreco();
+    
+        if (Marca.FORD.equals(carro.getMarca())){
+           return valorVenda.subtract(new BigDecimal(2000));
+       }
+        return proximoDesconto.aplicaDesconto(carro);
+    }
     
 }
